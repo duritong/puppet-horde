6aec0308a4d0f78a2394d92dcd62913292794599
mh <mh@immerda.ch>
2008-09-05 13:11:29 +0000

    added new php packages dependencies

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,6 +13,9 @@ class horde::base {
 
     include php
     include php::packages::geoip
+    include php::packages::lzf
+    include php::packages::services_weather
+    include php::packages::cache
 
     package{'horde':
         ensure => installed,

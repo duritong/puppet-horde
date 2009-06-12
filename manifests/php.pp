db19599ec5e2a100bd1703982800cb78459f2b23
mh <mh@immerda.ch>
2009-06-13 00:05:02 +0200

    include classes, rather than directly call the packages

--- manifests/php.pp
+++ manifests/php.pp
@@ -1,10 +1,13 @@
 # manifests/php.pp
 
 class horde::php {
-    include php
+  include php
 
-    php::pecl{ 'Fileinfo': }
-    php::pear{ [ 'MDB2', 'MDB2-Driver-pgsql', 'MDB2-Driver-mysql',
-                'Cache-Lite', 'Date-Holidays', 'XML-Serializer' ]:
-    }
+  include php::extensions::pecl::fileinfo
+  include php::extensions::pear::mdb2
+  include php::extensions::pear::mdb2::pgsql
+  include php::extensions::pear::mdb2::mysql
+  include php::extensions::pear::cache_lite
+  include php::extensions::pear::date_holidays
+  include php::extensions::pear::xml_serializer
 }

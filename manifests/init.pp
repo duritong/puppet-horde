505328062f479c5a38bcd32d8841fa7528b18a1d
mh <mh@immerda.ch>
2009-09-27 18:17:45 +0200

    factor things out in their own class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,32 +1,10 @@
-# modules/horde/manifests/init.pp - manage horde stuff
+# manage horde stuff
 # Copyright (C) 2007 admin@immerda.ch
 #
-
-import 'defines.pp'
 
 class horde {
     case $operatingsystem {
         centos: { include horde::centos }
         default: { include horde::base }
-    }
-}
-
-class horde::base {
-    include php
-    include php::packages::geoip
-    include php::packages::lzf
-    include php::packages::services_weather
-    include php::packages::cache
-
-    package{'horde':
-        ensure => installed,
-        require => Package['php'],
-    }
-}
-
-class horde::centos inherits horde::base {
-    package{'horde-enhanced':
-        ensure => installed,
-        require => Package['horde'],
     }
 }

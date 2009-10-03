4a6d6a1cca5964c2aeba217d839b7a3d65caf75c
mh <mh@immerda.ch>
2009-10-03 16:51:50 +0200

    add shorewall rules

--- manifests/imp.pp
+++ manifests/imp.pp
@@ -6,5 +6,7 @@ class horde::imp {
     horde::module{'imp': }
     if $use_shorewall {
         include shorewall::rules::out::keyserver
+        include shorewall::rules::out::imap
+        include shorewall::rules::out::pop3
     }
 }
